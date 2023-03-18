fa5b0b2e08daa42c625eca02bdabd88df0f4f9de
Richard Hanxu <richardhanxu@gmail.com>
2023-03-18 17:52:51 -0400

    Finalized model

--- inference.py
+++ inference.py
@@ -3,10 +3,10 @@ PATH_TO_LABELS = './labels.txt'
 
 import torch
 import torch.nn as nn
-import torchvision
 from torchvision import models, transforms
+from PIL import Image
 
-def predict_label(filename):
+def predict_label(fileobject):
     # Crop, center, normalize the image input
     transform = transforms.Compose([
                 transforms.CenterCrop(224),
@@ -20,11 +20,12 @@ def predict_label(filename):
     weights = torch.load(PATH_TO_WEIGHTS)
     resnet50.load_state_dict(weights)
 
-    # Steps to get an image output
-    # from PIL import Image
-    # input = Image.open('path_to_file')
-    # prediction = torch.argmax(resnet50(input.unsqueeze(0)), 1)
+    input = Image.open(fileobject)
+    prediction = torch.argmax(resnet50(input.unsqueeze(0)), 1)
 
-    # with open (PATH_TO_LABELS, 'r') as file:
-        # lines = file.readlines()
-        # predicted_label = lines[prediction]
+    with open (PATH_TO_LABELS, 'r') as file:
+        lines = file.readlines()
+        predicted_label = lines[prediction]
+
+    return predicted_label
+
